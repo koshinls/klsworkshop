f700c8f26c5fcaf09940acc7e2829823b4001587
koshinls <imkoshinLS@gmail.com>
2026-07-30 11:41:22 +0900

    Create item framework

---
+++ common/src/main/java/com/koshinls/klsworkshop/item/CreativeWrenchItem.java
@@ -0,0 +1,10 @@
+package com.koshinls.klsworkshop.item;
+
+import net.minecraft.world.item.Item;
+
+public class CreativeWrenchItem extends Item {
+
+    public CreativeWrenchItem(Properties properties) {
+        super(properties);
+    }
+}
---
+++ common/src/main/java/com/koshinls/klsworkshop/registry/ModItems.java
@@ -0,0 +1,4 @@
+package com.koshinls.klsworkshop.registry;
+
+public class ModItems {
+}
